6b2ef09ca3f35770353b71c765e9d7a6bcf95649
sainsh <kfsainsh@gmail.com>
2018-09-05 11:34:01 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String str = "mogens";
+        int variable = 24;
+        char bogstav = 'b';
+
+        System.out.println("Hello Datamatiker børn");
+        System.out.println(str);
+        System.out.println(5+2);
+        System.out.println(variable +3);
+        System.out.println(bogstav);
+    }
+
+}
+
