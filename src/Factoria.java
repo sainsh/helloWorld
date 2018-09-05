7b3e3a8324c73ecc03356cff2ec5d789f4a3040b
sainsh <kfsainsh@gmail.com>
2018-09-05 13:41:52 +0200

    comment

---
+++ src/Factoria.java
@@ -0,0 +1,18 @@
+public class Factoria {
+
+    public static void main(String[] args)
+    {
+        int i,number=25;
+        int factor=1;
+        for(i=1;i<=number;i++){
+
+            factor *=i;
+        }
+
+        System.out.println(factor);
+
+
+
+    }
+
+    }
