7bd96f757b16609ad5f785a4acbca395f7e6d6ee
sainsh <kfsainsh@gmail.com>
2018-09-05 12:11:06 +0200

    comment

--- src/IfExample.java
+++ src/IfExample.java
@@ -2,7 +2,15 @@ public class IfExample {
 public static void main(String[] args) {  
     int age=20;  
     if(age>18){  
-        System.out.print("Age is greater than 18");  
-    }  
+        System.out.print("Age is greater than 18 \n");
+    }
+    int number=13;
+    if(number%2==0){
+        System.out.println("even number");
+    }else{
+        System.out.println("odd number");
+    }
+
+
 }  
 } 
