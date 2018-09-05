98d1e2848ddb8e64adee4f51eaaeefd1b3bd966e
sainsh <kfsainsh@gmail.com>
2018-09-05 12:04:04 +0200

    comment

---
+++ src/OperatorExample.java
@@ -0,0 +1,14 @@
+class OperatorExample{  
+public static void main(String args[]){  
+int a=2;  
+int b=5;  
+int min=(a<b)?a:b;  
+System.out.println(min);
+
+     a=10;
+     b=5;
+     min=(a<b)?a:b;
+    System.out.println(min);
+
+    }
+}
